5241283f32c25cea8086dfa4f98c51e1b1937ff7
achoo-o <amyj.etana@gmail.com>
2024-10-30 16:52:40 +1300

    complete bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,21 @@
+def bubble_sort(num_arr)
+  sorted_arr = num_arr
+  swaps = 0.5
+  limit = sorted_arr.length - 2
+
+  until swaps == 0 do
+    swaps = 0
+    for i in 0..limit do
+      num = sorted_arr[i]
+      next_num = sorted_arr[i+1]
+      if num > next_num
+        sorted_arr.insert(i+1, sorted_arr.delete_at(i)) 
+        swaps += 1
+      end
+      puts "Loop ##{i}, array: #{sorted_arr}"
+    end
+  end
+  sorted_arr
+end
+
+puts bubble_sort([4,3,78,2,0,2])
